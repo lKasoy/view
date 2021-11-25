58a906b5267a965a66426dc116cd89b4b44518d4
lKasoy <sergeykosarevskiy@gmail.com>
2021-11-25 15:40:49 +0200

    view4

--- app/src/main/java/com/example/view/CustomView.kt
+++ app/src/main/java/com/example/view/CustomView.kt
@@ -1,11 +1,17 @@
 package com.example.view
 
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+
+val Int.dp: Float
+    get() = Resources.getSystem().displayMetrics.density * this
+val Int.sp: Float
+    get() = Resources.getSystem().displayMetrics.scaledDensity * this
 
 class CustomView @JvmOverloads constructor(
     context: Context,
@@ -14,15 +20,17 @@ class CustomView @JvmOverloads constructor(
 ) : View(context, attrs, defaultStyle) {
 
     private val radiusRounding: Float
-    private val p = Paint()
+    private val p = Paint().apply {
+        style = Paint.Style.STROKE
+    }
 
     init {
         context.theme.obtainStyledAttributes(
             attrs, R.styleable.CustomView, 0, 0
         ).apply {
             try {
-                radiusRounding = getDimension(R.styleable.CustomView_radiusRounding, 100f)
-                p.strokeWidth = getDimension(R.styleable.CustomView_lineWidth, 10f)
+                radiusRounding = getDimension(R.styleable.CustomView_radiusRounding, 40.dp)
+                p.strokeWidth = getDimension(R.styleable.CustomView_lineWidth, 8.dp)
                 p.color = getColor(R.styleable.CustomView_paintColor, Color.rgb(1, 2, 2))
             } finally {
                 recycle()
@@ -32,13 +40,15 @@ class CustomView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        val w = (this.measuredWidth - paddingRight - paddingLeft).toFloat()
-        val h = (this.measuredHeight - paddingBottom - paddingTop).toFloat()
+        val w = this.measuredWidth - paddingRight - paddingLeft - p.strokeWidth
+        val h = this.measuredHeight - paddingBottom - paddingTop - p.strokeWidth
+        val left = paddingLeft.toFloat() + p.strokeWidth/2
+        val top = paddingTop.toFloat() + p.strokeWidth/2
         canvas.drawRoundRect(
-            paddingLeft.toFloat(),
-            paddingTop.toFloat(),
-            w,
-            h,
+            left,
+            top,
+            left + w,
+            top + h,
             radiusRounding, radiusRounding, p
         )
     }
